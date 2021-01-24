8e693bddf485348a2d41e06650c1033b87403cc9
guvenelif39 <76227099+guvenelif39@users.noreply.github.com>
2021-01-24 19:04:30 +0300

    4. Hafta Ödevi - Dictionary

---
+++ Dictionaries/MyDict.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionaries
+{
+    class MyDict<T1,T2,T3>
+    {
+        T1[] idArray;
+        T2[] nameArray;
+        T3[] surnameArray;
+        T1[] idTemp;
+        T2[] nameTemp;
+        T3[] surnameTemp;
+
+        public MyDict()
+        {
+            idArray = new T1[0];
+            nameArray = new T2[0];
+            surnameArray = new T3[0];
+
+        }
+
+        public void Add(T1 id, T2 name, T3 surname)
+        {
+            idTemp = idArray;
+            nameTemp = nameArray;
+            surnameTemp = surnameArray;
+
+            idArray = new T1[idArray.Length + 1];
+            nameArray = new T2[nameArray.Length + 1];
+            surnameArray = new T3[surnameArray.Length + 1];
+            for (int i = 0; i < idTemp.Length; i++)
+            {
+                idArray[i] = idTemp[i];
+                nameArray[i] = nameTemp[i];
+                surnameArray[i] = surnameTemp[i];
+
+            }
+
+            idArray[idArray.Length - 1] = id;
+            nameArray[nameArray.Length - 1] = name;
+            surnameArray[surnameArray.Length - 1] = surname;
+
+        }
+
+        public int Length
+        {
+            get { return idArray.Length; }
+        }
+
+        public T1[] Ids
+        {
+            get { return idArray; }
+        }
+        public T2[] Names
+        {
+            get { return nameArray; }
+        }
+
+        public T3[] SurNames
+        {
+            get { return surnameArray; }
+        }
+
+        
+    }
+}
---
+++ Dictionaries/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDict<int, string, string> users = new MyDict<int, string, string> { };
+     
+            users.Add(1, "Elif", "Güven");
+            users.Add(2, "Deniz", "Çizgi");
+            users.Add(3, "Emir", "Korkmaz");
+            Console.WriteLine("Sistemde " + users.Length + " kullanıcı vardır.");
+            for (int i = 0; i < users.Length; i++)
+            {
+                Console.WriteLine(users.Ids[i] + " " + users.Names[i] + " " + users.SurNames[i]);
+            }
+
+            Console.WriteLine("------Sistemdeki kullanıcıların ID'leri------");
+            foreach (var id in users.Ids)
+            {
+                Console.WriteLine(id);
+            }
+
+            Console.WriteLine("------Sistemdeki kullanıcıların isimleri------ ");
+            foreach (var name in users.Names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("------Sistemdeki kullanıcıların soyisimleri------");
+            foreach (var surName in users.SurNames)
+            {
+                Console.WriteLine(surName);
+            }
+
+        }
+    }
+}
